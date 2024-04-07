8809c816c20679ef70a96e374f3fc0902fa7032d
Jackson <117477669+Jacksonkao97@users.noreply.github.com>
2024-04-06 23:03:25 -0500

    Add CollectionItem and Collection models

---
+++ manga-collection-app/src/app/model/collectionItemModel.d.ts
@@ -0,0 +1,8 @@
+interface CollectionItem {
+  id: string,
+  name: string,
+  creationDate: Date,
+  lastUpdated: Date,
+}
+
+export default CollectionItem
---
+++ manga-collection-app/src/app/model/collectionModel.d.ts
@@ -0,0 +1,9 @@
+interface Collection {
+  id: string,
+  name: string,
+  numberOfItems: number,
+  creationDate: string,
+  lastUpdated: string,
+}
+
+export default Collection

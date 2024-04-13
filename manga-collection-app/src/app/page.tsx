dd46be7f3a027882adbac207deb3051a903e4c91
Jackson <117477669+Jacksonkao97@users.noreply.github.com>
2024-04-12 19:07:21 -0500

    Update Home page layout and add NewsTable component

--- manga-collection-app/src/app/page.tsx
+++ manga-collection-app/src/app/page.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
+import NewsTable from './components/NewsTable';
+
 export default function Home() {
   return (
-    <>
-      <div>
-        Hi
+    <div className='flex flex-row justify-between gap-4'>
+      <div className='flex flex-col gap-3 w-1/2 h-news_table text-center'>
+        <h1 className='text-2xl font-bold'>News</h1>
+        <div className='drop-shadow-xl rounded-lg overflow-x-hidden overflow-y-auto'>
+          <NewsTable />
+        </div>
       </div>
-    </>
+      <div>
+        <h1>Image</h1>
+      </div>
+    </div>
   );
 }

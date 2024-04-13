7dbdbf19874cf5976297651b88e503bc8858f75a
Jackson <117477669+Jacksonkao97@users.noreply.github.com>
2024-04-12 19:07:26 -0500

    Update tailwind.config.ts with new height for news_table

--- manga-collection-app/tailwind.config.ts
+++ manga-collection-app/tailwind.config.ts
@@ -18,9 +18,31 @@ const config: Config = {
       },
       height: {
         collection_card: "450px",
+        news_table: "600px",
       },
     },
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["light"],
+          primary: "#524632",
+          secondary: "#8f7e4f",
+          accent: "#d8ffdd",
+          neutral: "#c3c49e",
+          "base-100": "#dedbd8"
+        }
+      },
+      "dark"],
+    darkTheme: "dark", // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
+  },
 };
 export default config;

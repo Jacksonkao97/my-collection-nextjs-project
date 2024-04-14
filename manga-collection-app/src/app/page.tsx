0e9149c3acfae59217199e2967dc1ee6523293bc
Jackson <117477669+Jacksonkao97@users.noreply.github.com>
2024-04-14 12:43:30 -0500

    Update globals.css to style scrollbars in Firefox, Chrome, Edge, and Safari

--- manga-collection-app/src/app/page.tsx
+++ manga-collection-app/src/app/page.tsx
@@ -1,18 +1,39 @@
-import React from 'react';
+import React from 'react'
 
-import NewsTable from './components/NewsTable';
+// Models
+import Collection from '@/app/model/collectionModel.d'
 
-export default function Home() {
+// Components
+import CollectionCard from './components/CollectionCard'
+import AddCollectionButton from './components/AddCollectionButton'
+
+interface FetchCollectionsResponse {
+  collections: Collection[]
+}
+
+export default async function Home() {
+  // Get all collections
+  const collections: Collection[] = await fetch(`${process.env.BASE_URL}/api/collections`, { cache: 'no-cache' })
+    .then(async (res) => {
+      if (!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status} and message: ${res.statusText}`)
+      }
+      const data: FetchCollectionsResponse = await res.json()
+      return data.collections
+    })
+    .catch(err => {
+      console.error(err.message)
+      throw new Error('Error in fetching collections')
+    })
+
   return (
-    <div className='flex flex-row justify-between gap-4'>
-      <div className='flex flex-col gap-3 w-1/2 h-news_table text-center'>
-        <h1 className='text-2xl font-bold'>News</h1>
-        <div className='drop-shadow-xl rounded-lg overflow-x-hidden overflow-y-auto'>
-          <NewsTable />
-        </div>
-      </div>
-      <div>
-        <h1>Image</h1>
+    <div className='flex flex-col gap-2 p-4'>
+      <div className='grid 2xl:grid-cols-5 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-12 place-items-center'>
+        {collections?.map((collection, index) => (
+          Object.keys(collection).length !== 0 ?
+            <CollectionCard collection={collection} key={index} /> : null
+        ))}
+        <AddCollectionButton />
       </div>
     </div>
   );

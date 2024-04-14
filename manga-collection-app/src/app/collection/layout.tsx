5819a7b70d3154445aecdddb30142783ad2224d5
Jackson <117477669+Jacksonkao97@users.noreply.github.com>
2024-04-14 12:53:09 -0500

    Add CollectionLayout and Collection components to manga-collection-app

---
+++ manga-collection-app/src/app/collection/layout.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Link from 'next/link'
+
+// Components
+import AddItemButton from '@/app/components/AddItemButton'
+
+const CollectionLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+  return (
+    <div className='flex flex-col gap-5'>
+      <div className='flex flex-row justify-between gap-4 min-w-max drop-shadow-xl'>
+        <Link className="btn btn-primary" href={
+          {
+            pathname: '/',
+          }
+        }>Back</Link>
+        <AddItemButton />
+      </div>
+      {children}
+    </div>
+  )
+}
+
+export default CollectionLayout
---
+++ manga-collection-app/src/app/collection/page.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+// Models
+import CollectionItem from '@/app/model/collectionItemModel'
+
+// Components
+import CollectionItemTable from '@/app/components/CollectionItemTable'
+
+interface SearchParams {
+  id: string,
+}
+
+interface FetchCollectionTableResponse {
+  collectionTable: CollectionItem[],
+}
+
+const Collection = async ({ searchParams }: { searchParams: SearchParams }) => {
+  const collectionId = searchParams.id
+  const table: CollectionItem[] = await fetch(`${process.env.BASE_URL}/api/collections/${collectionId}`, { cache: 'no-cache' })
+    .then(async (res) => {
+      if (!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status} and message: ${res.statusText}`)
+      }
+      const data: FetchCollectionTableResponse = await res.json()
+      return data.collectionTable
+    })
+    .catch(err => {
+      console.error(err.message)
+      throw new Error('Error fetching collection table')
+    })
+
+  return (
+    <CollectionItemTable table={table} />
+  )
+}
+
+export default Collection

3988e8a4b93a44b7e6ede0f2f4cba36319cc8813
Jackson <117477669+Jacksonkao97@users.noreply.github.com>
2024-04-17 10:51:02 -0500

    Refactor page component to use getCollectionList action

--- my-collection-app/src/app/page.tsx
+++ my-collection-app/src/app/page.tsx
@@ -2,25 +2,15 @@ import React from 'react'
 
 // Models
 import Collection from '@/app/model/collectionModel.d'
-import { FetchCollectionsResponse } from './model/apiReqResModel'
 
 // Components
 import CollectionList from './components/CollectionList'
 
+// actions
+import getCollectionList from './actions/getCollectionList'
+
 export default async function Home() {
-  // Get all collections
-  const collections: Collection[] = await fetch(`${process.env.BASE_URL}/api/collections`, { cache: 'no-cache' })
-    .then(async (res) => {
-      if (!res.ok) {
-        throw new Error(`HTTP error! status: ${res.status}`)
-      }
-      const data: FetchCollectionsResponse = await res.json()
-      return data.collections
-    })
-    .catch(err => {
-      console.error(err.message)
-      throw new Error(err.message)
-    })
+  const collections: Collection[] = await getCollectionList()
 
   return (
     <div className='flex flex-col gap-2'>
